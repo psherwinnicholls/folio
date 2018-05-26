51755103ca903aaf4d96bd6e46b7d8c389b5afdb
Phil <phil@decoded.com>
2018-05-26 13:59:47 +0100

    update the copy & hierarchy. tweak the responsive width and remove unused assets

--- js/main.js
+++ js/main.js
@@ -1,48 +1,41 @@
+var COLOURS = ["#9CAFD8", "#A7EBCA", "#F7CAC9", "#034F84", "#868E80"];
+var radius = 7;
 
+Sketch.create({
+  container: document.getElementById("container"),
+  autoclear: false,
 
-        var COLOURS = [ '#9CAFD8', '#A7EBCA', '#F7CAC9', '#034F84', '#868E80' ];
-        var radius = 7;
+  setup: function() {
+    console.log("setup");
+  },
 
-        Sketch.create({
+  update: function() {
+    radius = 2 + abs(sin(this.millis * 0.003) * 50);
+  },
 
-            container: document.getElementById( 'container' ),
-            autoclear: false,
+  // Event handlers
 
-            setup: function() {
-                console.log( 'setup' );
-            },
+  keydown: function() {
+    if (this.keys.C) this.clear();
+  },
 
-            update: function() {
-                radius = 2 + abs( sin( this.millis * 0.003 ) * 50 );
-            },
+  // Mouse & touch events are merged, so handling touch events by default
+  // and powering sketches using the touches array is recommended for easy
+  // scalability. If you only need to handle the mouse / desktop browsers,
+  // use the 0th touch element and you get wider device support for free.
+  touchmove: function() {
+    for (var i = this.touches.length - 1, touch; i >= 0; i--) {
+      touch = this.touches[i];
 
-            // Event handlers
+      this.lineCap = "round";
+      this.lineJoin = "round";
+      this.fillStyle = this.strokeStyle = COLOURS[i % COLOURS.length];
+      this.lineWidth = radius;
 
-            keydown: function() {
-                if ( this.keys.C ) this.clear();
-            },
-
-            // Mouse & touch events are merged, so handling touch events by default
-            // and powering sketches using the touches array is recommended for easy
-            // scalability. If you only need to handle the mouse / desktop browsers,
-            // use the 0th touch element and you get wider device support for free.
-            touchmove: function() {
-
-                for ( var i = this.touches.length - 1, touch; i >= 0; i-- ) {
-
-                    touch = this.touches[i];
-
-                    this.lineCap = 'round';
-                    this.lineJoin = 'round';
-                    this.fillStyle = this.strokeStyle = COLOURS[ i % COLOURS.length ];
-                    this.lineWidth = radius;
-
-                    this.beginPath();
-                    this.moveTo( touch.ox, touch.oy );
-                    this.lineTo( touch.x, touch.y );
-                    this.stroke();
-                }
-            }
-        });
-
-        
+      this.beginPath();
+      this.moveTo(touch.ox, touch.oy);
+      this.lineTo(touch.x, touch.y);
+      this.stroke();
+    }
+  }
+});
