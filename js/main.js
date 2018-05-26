e86de69ad0186c48c3a043702390abacb91fff93
Phil <phil@decoded.com>
2018-05-26 14:34:07 +0100

    remove div

--- js/main.js
+++ js/main.js
@@ -39,3 +39,15 @@ Sketch.create({
     }
   }
 });
+
+var colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+var i = 1;
+
+window.setInterval(function(){
+  document.body.style.backgroundColor = colors[i];
+  i++;
+  if (i === colors.length){
+    i=0;
+  }
+}, 2000);
